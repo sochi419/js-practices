c49d935bed499cd193a77a23f32814b7dec00137
sochi419 <equipment2241@gmail.com>
2023-02-05 17:09:40 +0900

    等価演算子の記法を修正

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,11 +1,11 @@
-for (let fizzbuzz = 1; fizzbuzz <= 20; fizzbuzz++){
-    if(fizzbuzz % 3 == 0 && fizzbuzz % 5 == 0){
-        console.log('FizzBuzz');
-    } else if (fizzbuzz % 3 == 0){
-        console.log('Fizz');
-    } else if (fizzbuzz % 5 == 0){
-        console.log('Buzz');
-    } else {
-        console.log(fizzbuzz);
-    }
+for (let fizzbuzz = 1; fizzbuzz <= 20; fizzbuzz++) {
+  if (fizzbuzz % 3 === 0 && fizzbuzz % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (fizzbuzz % 3 === 0) {
+    console.log("Fizz");
+  } else if (fizzbuzz % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(fizzbuzz);
+  }
 }
