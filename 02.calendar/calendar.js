ca16512f85da69d1caa008b0c26b069b6de280d8
sochi419 <equipment2241@gmail.com>
2023-02-08 18:45:01 +0900

    レビュー内容の修正

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -1,41 +1,41 @@
-let today = new Date();
+let calendar = (year, month) => {
+  // 引数が無い場合、今日の日付を取得。
+  if (process.argv.length == 2) {
+    const today = new Date();
+    year = today.getFullYear();
+    month = today.getMonth() + 1;
+  }
 
-let calendar = (year = today.getFullYear(), mon = today.getMonth() + 1) => {
-  let msg = "      " + mon + "月" + year + "年";
-  console.log(msg);
+  const message = `      ${month}月${year}年`;
+  console.log(message);
   console.log("日 月 火 水 木 金 土");
 
   // 月の初日の曜日を数字(0~6)で表す変数を定義
-  let day = new Date(year, mon - 1, 1);
-  let firstday = day.getDay();
+  const firstDayofWeek = new Date(year, month - 1, 1).getDay();
 
   // 月の末日を表す変数を定義
-  let date = new Date(year, mon, 0);
-  let lastdate = date.getDate();
+  const lastDayofMonth = new Date(year, month, 0).getDate();
 
-  for (let i = 0; i < firstday; i++) {
+  for (let i = 0; i < firstDayofWeek; i++) {
     process.stdout.write("   ");
   }
 
-  let count = 1;
-  while (count <= lastdate) {
+  for (let DayofMonth = 1; DayofMonth <= lastDayofMonth; DayofMonth++) {
     // 日にちが一桁の場合は、間隔調整のためにスペースを設けるif文を記載
-    if (count <= 9) {
-      process.stdout.write(" " + count + " ");
+    if (DayofMonth <= 9) {
+      process.stdout.write(` ${DayofMonth} `);
     } else {
-      process.stdout.write(count + " ");
+      process.stdout.write(`${DayofMonth} `);
     }
 
     // 土曜日で改行するif文を記載
-    let new_line = firstday + count;
-    if (new_line % 7 == 0) {
+    let judgeSaturday = firstDayofWeek + DayofMonth;
+    if (judgeSaturday % 7 == 0) {
       process.stdout.write("\n");
     }
-
-    count = count + 1;
   }
+  console.log();
 };
 
-let argv = require("minimist")(process.argv.slice(2));
-calendar(argv["y"], argv["m"]);
-console.log();
+const argv = require("minimist")(process.argv.slice(2));
+calendar(argv.y, argv.m);
