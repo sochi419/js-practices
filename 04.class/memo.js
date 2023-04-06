d0e261200b950cddd8e68b99de0219328b421a25
sochi419 <equipment2241@gmail.com>
2023-04-06 18:44:07 +0900

    データ型のクラスを作成して、オブジェクト指向に近づけた

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -3,85 +3,76 @@ const fs = require("fs");
 const { Select } = require("enquirer");
 
 class Memo {
-  listFiles() {
-    fs.readdir(".", (err, files) => {
-      const memo = pickMemo(files);
-      printFirstLine(memo);
-    });
-  }
+  constructor(files) {
+    const memo = [];
+    const MEMO_FILE_REGEXP = /^memo_\d+\.txt$/;
 
-  referFile() {
-    fs.readdir(".", async (err, files) => {
-      const memo = pickMemo(files);
-
-      const prompt = new Select({
-        name: "file",
-        message: "Choose file",
-        choices: memo,
-      });
-
-      const memoTitle = await prompt.run();
-      console.log(fs.readFileSync(memoTitle, "utf8"));
-    });
-  }
-
-  deleteFile() {
-    fs.readdir(".", async (err, files) => {
-      const memo = pickMemo(files);
-      printFirstLine(memo);
-
-      const prompt = new Select({
-        name: "file",
-        message: "Choose file",
-        choices: memo,
-      });
-
-      const memoTitle = await prompt.run();
-      fs.unlink(memoTitle, () => {});
-    });
-  }
-
-  addFile() {
-    const input = fs.readFileSync("/dev/stdin", "utf8");
-
-    const maxIndexJSON = fs.readFileSync("./max-index.json", "utf-8");
-    const maxIndexValue = JSON.parse(maxIndexJSON).max_index;
-    const newMaxIndex = { max_index: maxIndexValue + 1 };
-    const newMaxIndexJSON = JSON.stringify(newMaxIndex);
-
-    fs.writeFileSync(`./max-index.json`, newMaxIndexJSON);
-    fs.writeFileSync(`memo_${newMaxIndex.max_index}.txt`, input);
+    for (let file of files) {
+      if (fs.statSync(file).isFile() && MEMO_FILE_REGEXP.test(file)) {
+        memo.push(file);
+      }
+    }
+    this.memo = memo;
   }
 }
 
-const printFirstLine = (files) => {
-  files.forEach((file) => {
+class FirstLine {
+  constructor(file) {
     const text = fs.readFileSync(file, "utf8");
     const lines = text.toString().split("\r\n");
     const firstLine = lines[0].split(/\n/)[0];
 
-    console.log(firstLine);
-  });
-};
-
-const pickMemo = (files) => {
-  const memo = [];
-  const MEMO_FILE_REGEXP = /^memo_\d+\.txt$/;
-
-  for (let file of files) {
-    if (fs.statSync(file).isFile() && MEMO_FILE_REGEXP.test(file)) {
-      memo.push(file);
-    }
+    this.firstLine = firstLine;
   }
-  return memo;
-};
+}
 
 if (argv.l) {
-  new Memo().listFiles();
+  fs.readdir(".", (err, files) => {
+    const memos = new Memo(files)["memo"];
+
+    memos.forEach((memo) => {
+      const firstLine = new FirstLine(memo)["firstLine"];
+      console.log(firstLine);
+    });
+  });
 } else if (argv.r) {
-  new Memo().referFile();
+  fs.readdir(".", async (err, files) => {
+    const memos = new Memo(files)["memo"];
+
+    const prompt = new Select({
+      name: "file",
+      message: "Choose file",
+      choices: memos,
+    });
+
+    const memoTitle = await prompt.run();
+    console.log(fs.readFileSync(memoTitle, "utf8"));
+  });
 } else if (argv.d) {
-  new Memo().deleteFile();
+  fs.readdir(".", async (err, files) => {
+    const memos = new Memo(files)["memo"];
+    memos.forEach((memo) => {
+      const firstLine = new FirstLine(memo)["firstLine"];
+      console.log(firstLine);
+    });
+
+    const prompt = new Select({
+      name: "file",
+      message: "Choose file",
+      choices: memos,
+    });
+
+    const memoTitle = await prompt.run();
+    fs.unlink(memoTitle, () => {});
+  });
 } else {
-  new Memo().addFile();
+  const input = fs.readFileSync("/dev/stdin", "utf8");
+
+  const maxIndexJSON = fs.readFileSync("./max-index.json", "utf-8");
+  const maxIndexValue = JSON.parse(maxIndexJSON).max_index;
+  const newMaxIndex = { max_index: maxIndexValue + 1 };
+  const newMaxIndexJSON = JSON.stringify(newMaxIndex);
+
+  fs.writeFileSync(`./max-index.json`, newMaxIndexJSON);
+  fs.writeFileSync(`memo_${newMaxIndex.max_index}.txt`, input);
 }
